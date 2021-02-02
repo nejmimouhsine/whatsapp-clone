46daf990c2509d1a827dffd4907a47ed44832492
nejmimouhsine <nejmi.mouhcine10@gmail.com>
2021-02-02 12:19:15 +0100

    Whatsapp clone deployed to heroku and firebase

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Pusher from "pusher-js";
+
+import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
+import axios from "./components/axios";
+
+const App = () => {
+	const [messages, setMessages] = useState([]);
+
+	useEffect(() => {
+		axios.get("/messages/sync").then((response) => {
+			setMessages(response.data);
+		});
+	}, []);
+
+	useEffect(() => {
+		const pusher = new Pusher("b947162066bbbcd359fc", {
+			cluster: "eu",
+		});
+
+		const channel = pusher.subscribe("messages");
+		channel.bind("inserted", (newMessage) => {
+			setMessages([...messages, newMessage]);
+		});
+
+		return () => {
+			channel.unbind_all();
+			channel.unsubscribe();
+		};
+	}, [messages]);
+
+	console.log(messages);
+
+	return (
+		<div className="app">
+			<div className="app__body">
+				<Sidebar />
+				<Chat messages={messages} />
+			</div>
+		</div>
+	);
+};
+
+export default App;
